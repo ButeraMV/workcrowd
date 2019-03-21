02f0298c45bbce146358d47c9999b82190927afb
Boots2291 <buteramv@gmail.com>
2019-03-21 12:24:36 -0400

    Add project model

---
+++ app/assets/javascripts/project.js
@@ -0,0 +1,15 @@
+document.addEventListener('turbolinks:load', function() {
+  const openProjectButton = document.getElementById('new-project-button')
+  const projectPopover = document.getElementById('new-project-popover')
+
+  if (openProjectButton && projectPopover) {
+    openProjectButton.addEventListener('click', function() {
+      return projectPopover.classList.contains('is-hidden') ? projectPopover.classList.remove('is-hidden') : null
+    }, false)
+
+    const cancelProjectPopover = document.getElementById('cancel-project-popover')
+    cancelProjectPopover.addEventListener('click', function() {
+      return projectPopover.classList.add('is-hidden')
+    }, false)
+  }
+})
